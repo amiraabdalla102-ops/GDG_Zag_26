a923afa9e68c3016e9f1a13420d226800febc055
amiraabdalla102-ops <amiraabdalla102@gmail.com>
2026-03-04 23:44:01 +0200

    Add files via upload

---
+++ JSTask_1/task_6.js
@@ -0,0 +1,108 @@
+/* number1*/
+let NumberOne = 10 ;
+let NumberTwo = 20 ;
+console.log(NumberOne.toString() + NumberTwo.toString());
+console.log(typeof(NumberOne.toString() + NumberTwo.toString()));
+console.log(`${NumberOne}${NumberTwo}`);
+console.log(typeof(`${NumberOne}${NumberTwo}`));
+console.log(NumberTwo + "\n" + NumberOne);
+console.log(`${NumberTwo}
+${NumberOne}`);
+
+
+
+console.log(elzero.innerHTML);
+console.log(typeof elzero);
+
+
+console.log('Iam In' + "\n" + '//' + "\n" + 'Love // ""' + "\n" + '++ With ++' + "\n" + '\"""\"""' + "\n" + '""JavaScript""``')
+ 
+
+let a = 21;
+let b = 20;
+console.log('_'+a+'_'+b.toString() + a.toString()+'_'+b.toString() + a.toString()+'_'+b.toString() + a.toString()+'_'+b+'_')
+
+
+/* number2*/
+console.log(10 * 20 * 15 % 3 *190 * 10 * 400 )
+
+
+let num = 3;
+console.log(num + num);
+console.log(num * num - num);
+console.log(num * (true + true));
+console.log(num + true + true + true);
+console.log(num / num + true + true + true + true + true);
+console.log(num << true);
+
+
+let numbr = "10";
+console.log(Number(numbr) + Number(numbr));
+console.log(Number(numbr) << true);
+console.log(Number(numbr) * numbr.length);
+
+
+
+/* number3 */
+console.log(200000 - 100000);
+console.log(1000000 / 10);
+console.log(7e4 + 3e4);
+console.log(100 * 1000);
+console.log(100_000);
+
+
+
+console.log( Number.MIN_SAFE_INTEGER * -1);
+
+
+
+console.log(Number.MAX_SAFE_INTEGER.toString().length);
+
+
+
+let number = 10;
+console.log(number.toString().length);
+
+
+
+let flt = 10.4;
+console.log(Math.floor(flt));
+console.log(Math.round(flt));
+console.log(Math.ceil(flt));
+console.log(Math.trunc(flt));
+
+
+
+/* number4*/
+let userName = "Elzero";
+console.log(userName[0].toLowerCase());
+console.log(userName.slice(0,1).toLowerCase());
+console.log(userName.substring(0,1).toLowerCase());
+
+
+
+let word = "Elzero";
+let letterZ = "z";
+let letterE = "e";
+let letterO = "O";
+console.log(word.toLowerCase().includes(letterZ)); 
+console.log(word.toLowerCase().includes(letterE)); 
+console.log(word.toUpperCase().endsWith(letterO)); 
+
+
+
+
+/* number5*/
+ console.log(100 == "100"); 
+ console.log(100 < 1000); 
+ console.log(110 - 100 + 10 == 20); 
+ console.log(-10 == "-10"); 
+ console.log(! -50 >= +"-40"); 
+ console.log(! 10 <= -"-40"); 
+ console.log(! ("10" === 10));
+ console.log(! 20 == false); 
+
+
+
+
+
